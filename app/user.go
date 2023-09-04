c895eb1c9535a1b8e6c1a687e65858dee30808bf
ai-azz <82692915+ai-azz@users.noreply.github.com>
2023-09-04 10:45:05 +0700

    Update user.go

--- app/user.go
+++ app/user.go
@@ -1,13 +1,5 @@
 package app
 
-import "time"
-
 type User struct {
-    ID        uint      `json:"id" gorm:"primaryKey"`
-    Username  string    `json:"username" binding:"required"`
-    Email     string    `json:"email" binding:"required,unique"`
-    Password  string    `json:"password" binding:"required,min=6"`
-    Photos    []Photo   `json:"photos" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-    CreatedAt time.Time `json:"created_at"`
-    UpdatedAt time.Time `json:"updated_at"`
+	Email string
 }
